6d5870935b494e9956b3a8f4cdd0488329287d2b
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-01-27 21:04:37 +0700

    Create init carouselInner module scss

--- src/components/CarouselInner.jsx
+++ src/components/CarouselInner.jsx
@@ -1,22 +1,24 @@
+import styles from "./CarouselInner.module.scss";
+
 export function CarouselInner() {
   return (
     <>
-      <ul>
-        <li>
+      <ul className={styles.carousel_inner}>
+        <li className={styles.carousel_inner__item}>
           <img src="/images/image1.jpg" alt="image" />
           <div>
             <h1>Coba 1</h1>
             <p>Deskripsion</p>
           </div>
         </li>
-        <li>
+        <li className={styles.carousel_inner__item}>
           <img src="/images/image2.jpg" alt="image2" />
           <div>
             <h1>Coba 2</h1>
             <p>Deskripsion</p>
           </div>
         </li>
-        <li>
+        <li className={styles.carousel_inner__item}>
           <img src="/images/image3.jpg" alt="image3" />
           <div>
             <h1>Coba 3</h1>
