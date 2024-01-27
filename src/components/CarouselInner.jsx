e1cfaadb512596cde1b31cea5ab06499753794da
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-01-27 20:56:09 +0700

    Add images, create carouselInner component, and prepare the images

---
+++ src/components/CarouselInner.jsx
@@ -0,0 +1,15 @@
+export function CarouselInner() {
+  return (
+    <>
+      <ul>
+        <li>
+          <img src="/images/image1.jpg" alt="image" />
+          <div>
+            <h1>Coba</h1>
+            <p>Deskripsion</p>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+}
