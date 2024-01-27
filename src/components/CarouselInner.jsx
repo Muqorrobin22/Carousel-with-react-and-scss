6cb847d69af54abc2352d2dec1e1edca0aceb12f
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-01-27 21:29:23 +0700

    add class for manipulating text in carouselInner

--- src/components/CarouselInner.jsx
+++ src/components/CarouselInner.jsx
@@ -6,21 +6,21 @@ export function CarouselInner() {
       <ul className={styles.carousel_inner}>
         <li className={styles.carousel_inner__item}>
           <img src="/images/image1.jpg" alt="image" />
-          <div>
+          <div className={styles.carousel__item_text}>
             <h1>Coba 1</h1>
             <p>Deskripsion</p>
           </div>
         </li>
         <li className={styles.carousel_inner__item}>
           <img src="/images/image2.jpg" alt="image2" />
-          <div>
+          <div className={styles.carousel__item_text}>
             <h1>Coba 2</h1>
             <p>Deskripsion</p>
           </div>
         </li>
         <li className={styles.carousel_inner__item}>
           <img src="/images/image3.jpg" alt="image3" />
-          <div>
+          <div className={styles.carousel__item_text}>
             <h1>Coba 3</h1>
             <p>Deskripsion</p>
           </div>
