d47719bead66dd433d35ffb6cceb7b2d4824f5c7
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-01-27 22:52:47 +0700

    Functionality of carousel with useRef done

--- src/components/CarouselInner.jsx
+++ src/components/CarouselInner.jsx
@@ -1,16 +1,56 @@
+import { useRef } from "react";
 import styles from "./CarouselInner.module.scss";
 
 export function CarouselInner() {
+  const firstImage = useRef(null);
+  const secondImage = useRef(null);
+  const thirdImage = useRef(null);
+
+  console.log(firstImage);
+
+  function handleScrollFirstImage() {
+    firstImage.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  function handleScrollSecondImage() {
+    secondImage.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
+  function handleScrollThirdImage() {
+    thirdImage.current.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }
+
   return (
     <>
       <ul className={styles.carousel_inner}>
         <nav className={styles.carousel_inner__navigation}>
-          <div className={styles.nav_item}></div>
-          <div className={styles.nav_item}></div>
-          <div className={styles.nav_item}></div>
+          <div
+            className={styles.nav_item}
+            onClick={handleScrollFirstImage}
+          ></div>
+          <div
+            className={styles.nav_item}
+            onClick={handleScrollSecondImage}
+          ></div>
+          <div
+            className={styles.nav_item}
+            onClick={handleScrollThirdImage}
+          ></div>
         </nav>
         <li className={styles.carousel_inner__item}>
-          <img src="/images/image1.jpg" alt="image" />
+          <img src="/images/image1.jpg" alt="image" ref={firstImage} />
           <div className={styles.carousel__item_text}>
             <h1> First Slide </h1>
             <p>
@@ -22,7 +62,7 @@ export function CarouselInner() {
           </div>
         </li>
         <li className={styles.carousel_inner__item}>
-          <img src="/images/image2.jpg" alt="image2" />
+          <img src="/images/image2.jpg" alt="image2" ref={secondImage} />
           <div className={styles.carousel__item_text}>
             <h1>Second Slide</h1>
             <p>
@@ -34,7 +74,7 @@ export function CarouselInner() {
           </div>
         </li>
         <li className={styles.carousel_inner__item}>
-          <img src="/images/image3.jpg" alt="image3" />
+          <img src="/images/image3.jpg" alt="image3" ref={thirdImage} />
           <div className={styles.carousel__item_text}>
             <h1>third Slide</h1>
             <p>
