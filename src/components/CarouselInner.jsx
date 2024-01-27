2bf5cbd6d4e6297e604c4b6bfec47cb85497b8b2
Achmad Muqorrobin <achmadmuqorrobinassholih@gmail.com>
2024-01-27 20:56:39 +0700

    Add two images

--- src/components/CarouselInner.jsx
+++ src/components/CarouselInner.jsx
@@ -5,7 +5,21 @@ export function CarouselInner() {
         <li>
           <img src="/images/image1.jpg" alt="image" />
           <div>
-            <h1>Coba</h1>
+            <h1>Coba 1</h1>
+            <p>Deskripsion</p>
+          </div>
+        </li>
+        <li>
+          <img src="/images/image2.jpg" alt="image2" />
+          <div>
+            <h1>Coba 2</h1>
+            <p>Deskripsion</p>
+          </div>
+        </li>
+        <li>
+          <img src="/images/image3.jpg" alt="image3" />
+          <div>
+            <h1>Coba 3</h1>
             <p>Deskripsion</p>
           </div>
         </li>
